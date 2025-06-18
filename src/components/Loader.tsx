5aa330e95031802f76f95a3e740b926e21a2f659
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-18 23:01:08 +0200

    feat: Add Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <p className="text-center mt-10 text-2xl font-bold uppercase text-gray-300 tracking-wide animate-pulse">
+      Loading...
+    </p>
+  );
+}
