7a9e0fe516bfb6360beee5cf41742eb7c83a6b33
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-20 20:08:40 +0200

    feat: Add categories data

---
+++ src/data/categories.ts
@@ -0,0 +1,10 @@
+export const CATEGORIES = [
+  { name: "Fundamentals", color: "#1e3a8a" },
+  { name: "Frontend", color: "#166534" },
+  { name: "Backend", color: "#991b1b" },
+  { name: "Algorithm", color: "#ca8a04" },
+  { name: "Data Structure", color: "#9d174d" },
+  { name: "Network", color: "#0f766e" },
+  { name: "AI", color: "#c2410c" },
+  { name: "Math", color: "#6b21a8" },
+];
