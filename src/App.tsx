df9a81ab9903ad8fe6e06b08931ddde17c49abe0
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-20 20:10:11 +0200

    feat: Move data to Home component and set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,13 @@
-import { useEffect, useState } from "react";
-import { supabase } from "./lib/supabase.ts";
-import Header from "./components/Header.tsx";
-import AddTermForm from "./components/AddTermForm.tsx";
-import type { TermType } from "./types/term";
-import CategoryFilter from "./components/CategoryFilter.tsx";
-import TermList from "./components/TermList.tsx";
-import Loader from "./components/Loader.tsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 function App() {
-  const [currentCategory, setCurrentCategory] = useState<string>("all");
-  const [terms, setTerms] = useState<TermType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showForm, setShowForm] = useState(false);
-
-  useEffect(() => {
-    const fetchTerms = async () => {
-      setIsLoading(true);
-      const { data, error } = await supabase.from("terms").select("*").eq("approved", true);;
-      if (error) {
-        console.error("Error fetching terms:", error);
-      } else {
-        setTerms(data || []);
-      }
-      setIsLoading(false);
-    };
-
-    fetchTerms();
-  }, []);
-
-  const filteredTerms =
-    currentCategory === "all"
-      ? terms
-      : terms.filter((term) => term.tags.includes(currentCategory));
-
   return (
-    <>
-      <Header showForm={showForm} setShowForm={setShowForm} />
-      {showForm ? (
-        <AddTermForm setTerms={setTerms} setShowForm={setShowForm} />
-      ) : null}
-
-      <main className="main">
-        <CategoryFilter setCurrentCategory={setCurrentCategory} />
-
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <TermList terms={filteredTerms} setTerms={setTerms} />
-        )}
-      </main>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
 
