7f5f8f7320933250fb957dd36c573b387b8927a0
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-21 20:05:17 +0200

    style: Update styles

--- src/components/AdminBar.tsx
+++ src/components/AdminBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function AdminBar() {
   return (
-    <div className="fixed top-0 right-0 px-4 py-2 text-xs text-white z-50">
+    <div className="fixed top-0 right-0 z-50 bg-black bg-opacity-70 px-4 py-2 rounded-bl-md text-xs text-white shadow-md">
       <Link to="/admin" className="underline hover:text-gray-300">
         Admin
       </Link>
